e46308cdce543926254b8618fd9ba094270abc0a
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-09-14 11:45:53 -0500

    add ability to delete user

--- index.js
+++ index.js
@@ -210,7 +210,18 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
   }
 });
 
+//DELETE user
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
 
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    users = users.filter( user => user.id != id );
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send('they aint here');
+  }
 });
 
 //listens for requests
