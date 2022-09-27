67bd51a1df0c599092cbd8d0ef6851a91e3df6ce
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-09-27 14:57:26 -0500

    Mongoose user creation

--- index.js
+++ index.js
@@ -26,6 +26,39 @@ app.use(morgan('combined', {stream: accessLogStream}));
 
 app.use(express.static('public')); //routes static requests to the public folder
 
+//CREATE a user w mongoose
+/*JSON expected in this format
+{
+  ID: Integer,
+  UserName: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
+app.post('/users', (req, res) => {
+  Users.findOne({ UserName: req.body.UserName })
+  .then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.UserName + ' already exists');
+    } else {
+      Users
+        .create({
+          UserName: req.body.UserName,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        })
+        .then((user) =>{res.status(201).json(user) })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      })
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
 });
 
 //READ genre by genre title
