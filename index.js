6a805ed4add8f90509bceba748d2e868519409e5
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-09-29 13:49:58 -0500

    adjusted spacing to try to find error

--- index.js
+++ index.js
@@ -157,10 +157,7 @@ app.put('users/:UserName', (req, res) => {
     .catch((err) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
-    } else {
-      res.json(updatedUser);
-    }
-  });
+    });
 });
 
 //Add a favorite movie w mongoose
