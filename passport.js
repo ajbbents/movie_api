d12b31c2da7d35abeb987e8cca76190ea6c93a59
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-10-08 11:00:12 -0500

    adjusted per passport-jwt documentation

--- passport.js
+++ passport.js
@@ -4,8 +4,8 @@ const passport = require('passport'),
   passportJWT = require('passport-jwt');
 
 let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJWT;
+  JWTStrategy = require('passport-jwt').Strategy,
+  ExtractJWT = require('passport-jwt').ExtractJWT;
 
 passport.use(new LocalStrategy({
   usernameField: 'Username',
