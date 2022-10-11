7ca4c4a12e42793de0cdeaa0433f64895199ae8d
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-10-11 07:59:52 -0500

    fixed spellings

--- auth.js
+++ auth.js
@@ -8,23 +8,24 @@ let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.username, //this is the username being encoded in the JWT
     expiresIn: '7d',//specifies expiration
-    algorithm: 'HS256'//algorithm used to 'sign' or encode the valuse of the JWT
+    algorithm: 'HS256'//algorithm used to 'sign' or encode the values of the JWT
   });
 };
 
 //POST login
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
-      if (error || !user) {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+      if (err || !user) {
         return res.status(400).json({
           message: 'something is fishy here.',
           user: user,
         });
       }
-      req.login(user, { session: false }, (error) => {
-        if (error) {
-          res.send(error);
+      req.login(user, { session: false }, (err) => {
+        if (err) {
+          res.send(err);
+          console.log(err);
         }
         let token = generateJWTToken(user.toJSON());
         return res.json({ user, token });
