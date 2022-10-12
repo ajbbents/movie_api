0dd72dd2ab55267af865209359d6a22a87618f77
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-10-12 12:23:03 -0500

    adjusted spelling per model naming

--- passport.js
+++ passport.js
@@ -3,7 +3,7 @@ const passport = require('passport'),
   Models = require('./models.js'),
   passportJWT = require('passport-jwt');
 
-let Users = Models.User,
+let Users = Models.Users,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
