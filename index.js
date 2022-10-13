d8611b30cddfd03bc74187dce02905715e3da66c
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-10-13 13:36:24 -0500

    updated listening to heroku

--- index.js
+++ index.js
@@ -259,6 +259,7 @@ app.use((err, req, res, next) => {
 });
 
 //listens for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('listening on port ' + port);
 });
