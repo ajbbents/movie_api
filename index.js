6734025221b89f4121e6ef16adca4ff91a0ee1a5
ajbbents <107074385+ajbbents@users.noreply.github.com>
2022-11-04 13:50:44 -0500

    Removed /movies endpoint authentication temp.

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get('/', (req, res) => {
 });
 
 //Return all movies as JSON object
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', /* passport.authenticate('jwt', { session: false }), */(req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
